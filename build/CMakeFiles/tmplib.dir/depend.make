c3129d988c2244029aee2af8dd7ce61fef9b6a91
Dominik Suwala <d.suwala@student.uw.edu.pl>
2022-04-13 14:17:31 +0200

    directory cleaning

--- build/CMakeFiles/tmplib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tmplib.
-# This may be replaced when dependencies are built.
--- build/test/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominik/Documents/MISMaP/2021-22lato/computational_active_matter/c-crash-course-3-update-dsuwala/Assignment/assignment/test/googletest
-# Build directory: /home/dominik/Documents/MISMaP/2021-22lato/computational_active_matter/c-crash-course-3-update-dsuwala/Assignment/assignment/build/test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
